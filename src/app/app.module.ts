f319ebed652e1541e8e06c2042dc879f29aa5a73
Srinu Potnuru <iamsrinu3@gmail.com>
2021-01-26 23:32:02 +0530

    nav-bar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent} from './navbar/navbar.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+    NgbModule
   ],
+  exports:[BsDropdownModule,TooltipModule,ModalModule],
   providers: [],
   bootstrap: [AppComponent]
 })
