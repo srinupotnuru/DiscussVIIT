8829d869b3955e72e03878bc75e2717f3ade13b0
Srinu Potnuru <iamsrinu3@gmail.com>
2021-03-15 21:13:12 +0530

    sigin added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,38 +5,43 @@ import { Subject, Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'DiscussVIIT';
   call: Subject<Object> = new Subject<Object>();
   public user: Observable<Object> = this.call.asObservable();
-  constructor(private auth:AngularFireAuth,private fire:AngularFirestore){
-
+  constructor(private auth: AngularFireAuth, private fire: AngularFirestore) {
     this.auth.onAuthStateChanged((user) => {
       if (user) {
+        console.log("yoooo");
         sessionStorage.setItem('reload', 'false');
         this.setSession(user);
       }
-      
     });
   }
 
   async setSession(user) {
     let info: any = {};
+
     info['name'] = user.displayName;
     info['email'] = user.email;
     info['pic'] = user.photoURL;
     info['uid'] = user.uid;
+    info['qlist'] = [];
 
-    let val = await this.fire
-      .collection('config')
-      .doc('admins')
+    let flag = await this.fire
+      .collection('users')
+      .doc(user.email)
       .get()
       .toPromise();
+    console.log(flag.exists);
+    if (flag.exists == false) {
+      this.fire.collection('users').doc(user.email).set(info);
+    }
 
-      info = JSON.stringify(info);
-      sessionStorage.setItem('user', info);
-      this.call.next('reload');
+    info = JSON.stringify(info);
+    sessionStorage.setItem('user', info);
+    this.call.next('reload');
   }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,22 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import firebase from 'firebase/app';
 import { AppComponent } from '../app.component';
+import { AngularFirestore } from '@angular/fire/firestore';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor(public auth: AngularFireAuth , private app: AppComponent) {
-    this.auth.authState.subscribe(res=>{console.log(res)});
+  constructor(
+    public auth: AngularFireAuth,
+    private app: AppComponent,
+    private firestore: AngularFirestore
+  ) {
+    this.auth.authState.subscribe((res) => {
+      console.log(res);
+    });
     this.setUser();
     app.user.subscribe((go) => {
       this.setUser();
     });
   }
   authState: boolean = false;
-  user;
+  user: any;
   setUser() {
     let userOB = JSON.parse(sessionStorage.getItem('user'));
     if (userOB != null) {
@@ -28,16 +34,15 @@ export class NavbarComponent implements OnInit {
       this.authState = false;
     }
   }
-  login() {
-    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
-    this.authState=true;
+  async login() {
+    this.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+     
   }
   logout() {
+    sessionStorage.clear();
     this.auth.signOut();
-    this.authState=false;
+    window.location.reload();
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
