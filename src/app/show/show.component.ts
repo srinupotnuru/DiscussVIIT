ab695b2c45edb9f4c61d9832ac64b6150507880f
Srinu Potnuru <iamsrinu3@gmail.com>
2021-05-09 14:59:47 +0530

    comments added

--- src/app/show/show.component.ts
+++ src/app/show/show.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Location} from '@angular/common';
+import { AngularFirestore } from '@angular/fire/firestore';
+import {Router} from '@angular/router';
+import { AppComponent } from '../app.component';
+import { AngularFireAuth } from '@angular/fire/auth';
 @Component({
   selector: 'app-show',
   templateUrl: './show.component.html',
@@ -7,12 +11,36 @@ import {Location} from '@angular/common';
 })
 export class ShowComponent implements OnInit {
   data:any={};
-  constructor(private loc:Location) {
-        this.data=sessionStorage.getItem("item");
+  
+   constructor(
+    public auth: AngularFireAuth,
+    private app: AppComponent,
+    private firestore: AngularFirestore,
+    private route:Router,
+    private loc:Location
+  ) {
+    this.data=sessionStorage.getItem("item");
         this.data=JSON.parse(this.data);
-        
-       
-   }
+    this.auth.authState.subscribe((res) => {
+      
+    });
+    this.setUser();
+    app.user.subscribe((go) => {
+      this.setUser();
+    });
+  }
+  authState: boolean = false;
+  user: any;
+  setUser() {
+    let userOB = JSON.parse(sessionStorage.getItem('user'));
+    if (userOB != null) {
+      this.user = userOB;
+      this.authState = true;
+    } else {
+      this.user = null;
+      this.authState = false;
+    }
+  }
 
   ngOnInit(): void {
   }
